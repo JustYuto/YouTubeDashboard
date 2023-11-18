2b8b523b6ab673acabdafb58b738fb41f1e282b5
Kong-Yin-Ying <107056124+Kong-Yin-Ying@users.noreply.github.com>
2023-11-18 17:50:02 +0800

    google Outh

--- youtubedashboard/src/main.js
+++ youtubedashboard/src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import vue3GoogleLogin from 'vue3-google-login'
 
-createApp(App).mount('#app')
+const CLIENT_ID = '785497567658-16251n3ml1bu0mp440s4krbsi25obke7.apps.googleusercontent.com'
+
+createApp(App)
+  .use(vue3GoogleLogin, {
+    clientId: CLIENT_ID,
+    //scope: 'profile email',
+    //prompt: 'select_account'
+  })
+.mount('#app');
